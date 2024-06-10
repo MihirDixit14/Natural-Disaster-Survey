a0d5529070e863b5271b03e42cc22ed0be9faad4
MihirDixit14 <mdixit@uwm.edu>
2024-06-10 13:23:05 -0400

    First

---
+++ Disasters.py
@@ -0,0 +1,20 @@
+# %%
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import csv
+
+df=pd.read_csv(r'C:\Users\mihir\Downloads\disaster-events new.csv')
+print(df.head())
+print(df.columns)
+print(df.get('Earthquake'))
+
+
+
+# %%
+print(df.Disasters)
+print(df.Entity)
+print(df.Year)
+print(df.get('Wildfires'))
+
